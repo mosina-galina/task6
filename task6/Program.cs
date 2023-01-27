d0b1a191988ef7a15a6447684c55f703801b25f7
Galina <galinka-ver@mail.ru>
2023-01-27 22:40:00 +0500

    Числа больше 0  в массиве

---
+++ task6/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите длинну массива ");
+int m = int.Parse(Console.ReadLine() ?? "");
+int[] array = new int[m];
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine("Введите число ");
+    array[i] = int.Parse(Console.ReadLine() ?? "");
+}
+int p = 0;
+for (int k = 0; k < m; k++)
+{
+     if (array[k]>0) p++;
+}
+for (int j = 0; j < m; j++)
+{
+    Console.Write($"{array[j]} ");
+}
+Console.Write($" Количество чисел больше 0 = {p}");
---
+++ task6/prog.cs
@@ -0,0 +1,18 @@
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите длинну массива ");
+int m = int.Parse(Console.ReadLine() ?? "");
+int[] array = new int[m];
+for (int i = 0; i < m; i++)
+{
+    array[i] = Console.ReadLine("Введите число ");
+}
+int p = 0;
+for (int k = 0; k < m; k++)
+{
+     if (array[k]>0) p++;
+}
+for (int j = 0; j < m; i++)
+{
+    Console.Write($"{array[j]} ");
+}
+Console.Write($" Количество чисел больше 0 = {p}");
